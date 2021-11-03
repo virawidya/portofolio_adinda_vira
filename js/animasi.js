81897b4cbf25b68b5314fcd6811a5d543bf83533
virawidya <93634450+virawidya@users.noreply.github.com>
2021-11-03 11:15:19 +0700

    upload Portfolio Saya!
    Mengupload Portfolio

---
+++ js/animasi.js
@@ -0,0 +1,46 @@
+const txtElement = ['Wanita yang Lemah Iman,yang selalu tergoda Buaya Darat ' ];
+let count = 0;
+let txtIndex = 0;
+let currentTxt = '';
+let words = '';
+
+(function ngetik(){
+
+	if(count == txtElement.length){
+		count = 0;
+	}
+
+	currentTxt = txtElement[count];
+
+	words = currentTxt.slice(0, ++txtIndex);
+	document.querySelector('.efek-ngetik').textContent = words;
+
+	if(words.length == currentTxt.length){
+		count++;
+		txtIndex = 0;
+	}
+
+	setTimeout(ngetik, 500);
+
+})();
+
+/* Tombol Scroll Ke Atas */
+
+mybutton = document.getElementById("top-btn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
